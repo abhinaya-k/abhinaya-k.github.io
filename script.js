c96548fe0768771e0088f6c28bde5fca392be95a
abhinaya-k <abhinaya.k@spintly.com>
2026-02-22 09:02:08 +0530

    add password generator tool

--- script.js
+++ script.js
@@ -31,10 +31,7 @@ function generateID() {
 
   lastTimestamp = timestamp;
 
-  const id =
-    (timestamp << timeShift) |
-    (nodeID << nodeShift) |
-    sequence;
+  const id = (timestamp << timeShift) | (nodeID << nodeShift) | sequence;
 
   return id;
 }
@@ -43,3 +40,40 @@ document.getElementById("generateBtn").addEventListener("click", () => {
   const id = generateID();
   document.getElementById("result").innerText = id.toString();
 });
+
+function generatePassword(length) {
+  let chars = "";
+  if (optUpper.checked) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (optLower.checked) chars += "abcdefghijklmnopqrstuvwxyz";
+  if (optNumber.checked) chars += "0123456789";
+  if (optSymbol.checked) chars += "!@#$%^&*()_+-=";
+
+  if (optSimilar.checked) {
+    chars = chars.replace(/[O0Il1]/g, "");
+  }
+
+  if (!chars) return "";
+
+  let pwd = "";
+  const array = new Uint32Array(length);
+  crypto.getRandomValues(array);
+  for (let i = 0; i < length; i++) {
+    pwd += chars[array[i] % chars.length];
+  }
+  return pwd;
+}
+
+function updateStrength(pwd) {
+  let score = 0;
+  if (pwd.length >= 12) score++;
+  if (/[A-Z]/.test(pwd)) score++;
+  if (/[a-z]/.test(pwd)) score++;
+  if (/[0-9]/.test(pwd)) score++;
+  if (/[^A-Za-z0-9]/.test(pwd)) score++;
+
+  const percent = (score / 5) * 100;
+  strengthBar.style.width = percent + "%";
+
+  const labels = ["Very Weak", "Weak", "Moderate", "Strong", "Very Strong"];
+  strengthText.textContent = "Strength: " + labels[Math.max(score - 1, 0)];
+}
